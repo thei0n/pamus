10471cc541e01f6c9152fa51c555465a7c48f835
theion <pheion1@proton.me>
2023-10-05 17:26:14 +0400

    install script was modified

--- install.sh
+++ install.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
+default_file()
+{
+touch ~/.config/pamus/music.txt  
+	echo 'M5YXXU0rKjc Happy_Nation Pop Ace_of_Base Happy_Nation' >> ~/.config/pamus/music.txt
+	echo 'All done:
+		"Happy Nation by Ace of Base" is add to the config file.
+		To download it run
+			pamus -S
+		To tag it run
+			pamus -T '
+
+
+}
+
 mkdir -p ~/.config/pamus 
 mkdir -p ~/.config/pamus/functions
 
 cp ./functions.sh ~/.config/pamus/functions/functions.sh
 sudo cp main.sh /usr/local/bin/pamus
 
-if [ -z ~/.config/pamus/music.txt ]
-then
-	touch ~/.config/pamus/music.txt  
-	echo 'M5YXXU0rKjc Happy_Nation Pop Ace_of_Base Happy_Nation' >> ~/.config/pamus/music.txt
-fi
-
-echo 'All done:
-	"Happy Nation by Ace of Base" is add to the config file.
-	To download it run
-		pamus -S
-	To tag it run
-		pamus -T '
+[ ! -f ~/.config/pamus/music.txt ] && default_file
+	
 
 
- 
